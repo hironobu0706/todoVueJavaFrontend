c34f331dc90d878de98dfd9eca6bdd60ad1059b2
obqd3614@yahoo.co.jp <obqd3614@yahoo.co.jp>
2026-04-16 08:42:01 +0900

    ログイン画面の入力フォーム作成、バリデーション実装まで完了

--- src/components/pages/login.vue
+++ src/components/pages/login.vue
@@ -23,12 +23,15 @@ function onSubmit() {
 // 検証スキーマ
 const schema = yup.object({
   mailAddress: yup.string().email().required(),
+  password: yup.string().required().min(8).max(12),
 });
 const { values, errors, defineField } = useForm({
   validationSchema: schema,
 });
-
-const [mailAddress, mailAddressAttrs] = defineField('mailAddress',{
+const [mailAddress, mailAddressAttrs] = defineField('mailAddress', {
+  validateOnModelUpdate: false,
+})
+const [password, passwordAttrs] = defineField('password', {
   validateOnModelUpdate: false,
 })
 
@@ -38,13 +41,44 @@ const [mailAddress, mailAddressAttrs] = defineField('mailAddress',{
   <div class="content-area">
     <h1>ログイン画面</h1>
     <div>ここに説明等を記載</div>
+
+    <v-row justify="center" align="center" class="mt-5">
+      <v-col cols="3" align="right">
+        <label>メールアドレス</label>
+      </v-col>
+      <v-col cols="9">
+        <input type="email" class="form-control" v-model="mailAddress"  v-bind="mailAddressAttrs"/>
+        <!-- <input v-model="mailAddress" v-bind="mailAddressAttrs" />
+      <pre>values:{{ values.mailAddress }}</pre> -->
+      </v-col>
+    </v-row>
+    <v-row gap="0">
+      <v-col cols="3">
+      </v-col>
+      <v-col cols="9">
+        <p>{{ errors.mailAddress }}</p>
+      </v-col>
+    </v-row>
+    <v-row justify="center" align="center">
+      <v-col cols="3" align="right">
+        <label>パスワード</label>
+      </v-col>
+      <v-col cols="9">
+        <input type="password" class="form-control" v-model="password"  v-bind="passwordAttrs" />
+        <!-- <input v-model="password" v-bind="passwordAttrs" />
+      <pre>values:{{ values.password }}</pre>-->
+        <!-- <div>{{ errors.password }}</div> -->
+      </v-col>
+    </v-row>
+    <v-row gap="0">
+      <v-col cols="3">
+      </v-col>
+      <v-col cols="9">
+        <p>{{ errors.password }}</p>
+      </v-col>
+    </v-row>
     <div class="button-container">
       <br />
-
-      <input v-model="mailAddress" v-bind="mailAddressAttrs" />
-
-      <pre>values:{{ values.mailAddress }}</pre>
-      <pre>errors: {{ errors.mailAddress }}</pre>
     </div>
   </div>
 </template>
